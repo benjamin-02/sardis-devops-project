d5a04f0329ce7663bd9f1be9878330c23e47e052
Bünyamin <bunyamin.ozkaya@outlook.com>
2024-03-06 21:45:05 +0100

    add improvement on frontend design and take the backend url as environemt variable

--- front_end/public/script.js
+++ front_end/public/script.js
@@ -1,10 +1,22 @@
 // script.js
 async function getQuote() {
+    // Show loading spinner
+    document.getElementById('loader').style.display = 'block';
+    
     try {
-        const response = await fetch('http://sardis-be-svc:5000/quote');
+        // Get backend URL from environment variable SARDIS_BACKEND_URL or default to 'http://sardisbackend:5000/quote'
+        const backendUrl = process.env.SARDIS_BACKEND_URL || 'http://sardisbackend:5000/quote';
+        const response = await fetch(backendUrl);
         const data = await response.json();
-        document.getElementById('quote').textContent = data.quote;
+        // Hide loading spinner
+        document.getElementById('loader').style.display = 'none';
+        // Update quote placeholder with fetched quote
+        document.getElementById('quote-placeholder').textContent = data.quote;
     } catch (error) {
         console.error('Error fetching quote:', error);
+        // Hide loading spinner
+        document.getElementById('loader').style.display = 'none';
+        // Display error message
+        document.getElementById('quote-placeholder').textContent = 'Error fetching quote. Please try again later.';
     }
 }
