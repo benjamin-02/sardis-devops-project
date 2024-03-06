643df66d8470e2235f6313cf96efe628160f154a
Bünyamin <bunyamin.ozkaya@outlook.com>
2024-03-06 23:36:14 +0100

    typo fix

--- front_end/public/script.js
+++ front_end/public/script.js
@@ -4,8 +4,8 @@ async function getQuote() {
     document.getElementById('loader').style.display = 'block';
     
     try {
-        // Get backend URL from environment variable SARDIS_BACKEND_URL or default to 'http://sardisbackend:5000/quote'
-        const backendUrl = process.env.SARDIS_BACKEND_URL || 'http://sardisbackend:5000/quote';
+        // Get backend URL from environment variable SARDIS_BACKEND_URL
+        const backendUrl = process.env.SARDIS_BACKEND_URL;
         const response = await fetch(backendUrl);
         const data = await response.json();
         // Hide loading spinner
