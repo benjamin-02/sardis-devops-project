54ed57245356d05fca465e9fe63d9da1e27300b7
Harun Zafer <harunzafer.dev@gmail.com>
2024-03-18 07:21:38 -0400

    Implement client side config from environment variables

--- front_end/public/script.js
+++ front_end/public/script.js
@@ -3,14 +3,14 @@
 async function getQuote() {
     // Show loading spinner
     document.getElementById('loader').style.display = 'block';
-    
+    const apiUrl = '%%API_URL%%';
     try {
         // 1.version: Get backend URL from environment variable SARDIS_BACKEND_IP
         // const backendUrl = `${process.env.SARDIS_BACKEND_IP}:5000/quote`;
         // 2.version: Get the url from config.js with importing it:
         // const response = await fetch(backendUrl);
         // 3. version: hardcoded:
-        const response = await fetch('http://REPLACE-THIS:5000/quote');
+        const response = await fetch(`http://${apiUrl}:5000/quote`);
         const data = await response.json();
         // Hide loading spinner
         document.getElementById('loader').style.display = 'none';
